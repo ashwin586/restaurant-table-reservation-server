7f0e849c580f000bd64166cff804e68d2f975a70
Ashwin <ashwinv586@gmail.com>
2023-10-09 22:01:02 +0530

    protected the current admin routes

--- src/interfaces/routes/adminRoutes.js
+++ src/interfaces/routes/adminRoutes.js
@@ -1,13 +1,14 @@
 import {Router} from  'express';
 import { adminLogin } from '../controllers/adminController/AdminAuthController.js';
 import { findUsers, blockUser, unBlockUser } from '../controllers/adminController/adminUserManagment.js';
+import { adminCheck } from '../../middlewares/jwtAuth.js';
 
 const adminRoutes = Router()
 
 adminRoutes.post('/login', adminLogin);
-adminRoutes.get('/getUserData', findUsers);
+adminRoutes.get('/getUserData', adminCheck, findUsers);
 
-adminRoutes.put('/blockUser', blockUser);
-adminRoutes.put('/unBlockUser', unBlockUser);
+adminRoutes.put('/blockUser', adminCheck, blockUser);
+adminRoutes.put('/unBlockUser', adminCheck, unBlockUser);
 
 export default adminRoutes;
--- src/middlewares/jwtAuth.js
+++ src/middlewares/jwtAuth.js
@@ -1,9 +1,25 @@
 import Jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+dotenv.config();
 
 export const generateAdminToken = async(email) => {
     try{
         return Jwt.sign(email, process.env.JWTSECRETKEY);
     }catch(err){
         console.log(err)
+    }
+}
+
+export const adminCheck = async(req, res, next) =>{
+    const token = req.headers.authorization?.trim().split(' ')[1];
+    if(!token){
+        res.status(401).json({message: 'Unauthorised Access'});
+    } else {
+        try{
+            Jwt.verify(token, process.env.JWTSECRETKEY);
+            next();
+        }catch(err){
+            console.log(err);
+        }
     }
 }
